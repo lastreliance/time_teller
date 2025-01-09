2b080de966821de52d23893075c3e1b2bc10bf9d
lastreliance <chinger1231@gmail.com>
2025-01-09 13:08:52 +0800

    homepage

--- api.py
+++ api.py
@@ -2,6 +2,12 @@ import time
 import flask.views
 
 
+class Homepage(flask.views.MethodView):
+    @staticmethod
+    def get():
+        return flask.render_template("homepage.html")
+
+
 class TimeTeller(flask.views.MethodView):
     @staticmethod
     def get():
